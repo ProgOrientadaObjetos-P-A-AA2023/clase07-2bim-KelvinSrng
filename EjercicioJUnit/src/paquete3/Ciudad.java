c726bdf8e5e9fb358f82fb0e862088c424061c58
KelvinSrng <116174502+KelvinSrng@users.noreply.github.com>
2023-07-20 11:44:45 -0500

    clase 07

--- EjercicioJUnit/src/paquete3/Ciudad.java
+++ EjercicioJUnit/src/paquete3/Ciudad.java
@@ -10,6 +10,36 @@ package paquete3;
  * @author reroes
  */
 public class Ciudad {
+    private String nombre;
+    private int poblacion;
+    
+    public void establecerNombre(String n){
+        nombre = n;
+    }
+    
+    public String obtenerNombre(){
+        return nombre;
+    }
+    
+    public void establecerPoblacion(int n){
+        poblacion = n;
+    }
+    
+    public int obtenerPoblacion(){
+        return poblacion;
+    }
+    
+    
+    public String toString(){
+        String cadena;
+        
+        cadena = String.format("Ciudad de Ecuador\n Nombre: %s\n\n\t"
+                + "Población: %d\n"  ,nombre, poblacion);
+        
+        return cadena;
+    }
+    
+    
     
     
 }
